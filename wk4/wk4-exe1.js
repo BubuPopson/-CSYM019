1cf0267247933117eb449dce963c5b66eafebabb
BubuPopson <reachbusola@yahoo.com>
2022-03-06 19:52:12 +0100

    wk4-exe4 first commit web development

--- wk4/wk4-exe1.js
+++ wk4/wk4-exe1.js
@@ -7,3 +7,12 @@ function print5() {
    }
    print5();
    print5();
+
+   function myLoadFunction() {
+    var element = document.getElementById('pageheading');
+    element.firstChild.nodeValue = 'New Heading';
+    var element = document.getElementById('paragraph');
+    element.firstChild.nodeValue = 'New paragraph text';
+   }
+   document.addEventListener('DOMContentLoaded', myLoadFunction);
+   
