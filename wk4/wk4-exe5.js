aee635f5c9aaf526563cb6b1ef800bf17e51a70d
BubuPopson <reachbusola@yahoo.com>
2022-03-14 13:37:14 +0100

    final commit to wk4 exercise 5

---
+++ wk4/wk4-exe5.js
@@ -0,0 +1,7 @@
+function myLoadFunction() {
+    var h1_element = document.getElementById('pageheading');
+    h1_element.firstChild.nodeValue = 'New Heading';
+    var p_element = document.getElementById('paragraph');
+    p_element.firstChild.nodeValue = 'New paragraph text';
+   }
+ document.addEventListener('click', myLoadFunction);
