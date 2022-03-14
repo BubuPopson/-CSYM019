efca0fd36d2893d4dc477be7ba9b9eff9add58ee
BubuPopson <reachbusola@yahoo.com>
2022-03-14 12:57:08 +0100

    final comment to wk4 exercise 4

--- wk4/wk4-exe4.js
+++ wk4/wk4-exe4.js
@@ -1,8 +1,8 @@
 function myLoadFunction() {
-    var element = document.getElementById('pageheading');
-    element.firstChild.nodeValue = 'New Heading';
-    var element = document.getElementById('paragraph');
-    element.firstChild.nodeValue = 'New paragraph text';
+    var h1_element = document.getElementById('pageheading');
+    h1_element.firstChild.nodeValue = 'New Heading';
+    var p_element = document.getElementById('paragraph');
+    p_element.firstChild.nodeValue = 'New paragraph text';
    }
    document.addEventListener('DOMContentLoaded', myLoadFunction);
    
